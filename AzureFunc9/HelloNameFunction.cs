f707d997f5b820f3ed7d7a886ed05d00d4921db9
Darmidont <ddarmidont@gmail.com>
2026-01-06 14:40:41 +0200

    Initial commit

---
+++ AzureFunc9/HelloNameFunction.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunc9;
+
+public class HelloNameFunction
+{
+    private readonly ILogger<HelloNameFunction> _logger;
+
+    public HelloNameFunction(ILogger<HelloNameFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HelloNameFunction")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        string name = req.Query["name"];
+        return name != null
+            ? new OkObjectResult($"Hello, {name}!")
+            : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+    }
+}
